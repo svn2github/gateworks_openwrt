6c9a0bb12aedaeda270c26aa3d332ce85b63ed77
tharvey <tharvey@918123d7-9a9f-4a10-9424-6605889f5124>
2012-03-10 02:06:42 +0000

    implement a 'tivideo' luci web-admin module (admin/status/tivideo) which adds a 'Video' tab to the Status menu showing the status of the service configuration (Video encoder vs decoder as well as stream parameters) and offers a downloadable SDP file to launch the stream in a player supporting sdp files (vlc,mplayer,totem, etc)
    remove the old implementation is not compatible with modern luci
    
    
    git-svn-id: http://svn.gateworks.com/openwrt@222 918123d7-9a9f-4a10-9424-6605889f5124

--- trunk/packages/gstreamer/ti-video/rootfs/usr/lib/lua/luci/controller/tivideo.lua
+++ trunk/packages/gstreamer/ti-video/rootfs/usr/lib/lua/luci/controller/tivideo.lua
@@ -1,17 +1,3 @@
---[[
-LuCI - Lua Configuration Interface
-
-Copyright 2008 Steven Barth <steven@midlink.org>
-Copyright 2008 Jo-Philipp Wich <xm@leipzig.freifunk.net>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-$Id: tivideo.lua 5118 2009-07-23 03:32:30Z jow $
-]]--
 module("luci.controller.tivideo", package.seeall)
 
 function index()
@@ -19,6 +5,6 @@ function index()
 		return
 	end
 	
-	local page = entry({"admin", "services", "tivideo"}, cbi("tivideo/tivideo"), "Video", 99)
-	page.dependent = false
+	entry({"admin", "status", "tivideo"}, template("tivideo"), "Video", 99).dependent = false;
+	entry({"admin", "services", "tivideo"}, cbi("tivideo/tivideo"), "Video", 99).dependent = false;
 end
